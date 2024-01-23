ee286f1353de710430fba2ea7557e990ccdb853f
fewolee <chulwoo1304@gmail.com>
2024-01-23 20:33:23 +0900

    Security Config 작성, 회원가입 구현

---
+++ src/main/java/com/example/testsecurity/service/JoinService.java
@@ -0,0 +1,38 @@
+package com.example.testsecurity.service;
+
+import com.example.testsecurity.dto.JoinDto;
+import com.example.testsecurity.entity.UserEntity;
+import com.example.testsecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+@Service
+public class JoinService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    public void joinProcess(JoinDto joinDto) {
+
+
+        //db에 이미 동일한 username을 가진 회원이 존재하는지?
+        boolean isUser = userRepository.existsByUsername(joinDto.getUsername());
+        if (isUser) {
+            return;
+        }
+
+
+        UserEntity data = new UserEntity();
+
+        data.setUsername(joinDto.getUsername());
+        data.setPassword(bCryptPasswordEncoder.encode(joinDto.getPassword()));
+        data.setRole("ROLE_USER");
+
+
+        userRepository.save(data);
+    }
+}
